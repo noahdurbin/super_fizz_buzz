e23d7b7d8ea57b73c84c648bdebcd99f7723c0be
Noah Durbin <noahdurbin@gmail.com>
2024-03-27 12:13:21 -0600

    worked on superfizzbuzz an dmade it to iteration two

--- lib/super_fizz_buzz.rb
+++ lib/super_fizz_buzz.rb
@@ -1,2 +1,28 @@
-# Iterate through the numbers 1 to 1000
-# For each one, print the output according to the rules of Super Fizz Buzz
+class SuperFizzBuzz
+
+    attr_reader :nums
+
+    def initialize(nums = nil)
+        @nums = [nums]
+    end
+
+    def output(nums)
+        nums.each do |current_number|
+            if current_number % 3 == 0 && current_number % 5 == 0 && current_number % 7 == 0
+                puts 'SuperFizzBuzz'
+            elsif current_number % 5 == 0 && current_number % 7 == 0
+                puts'SuperBuzz'
+            elsif current_number % 3 == 0 && current_number % 5 == 0
+                puts 'FizzBuzz'
+            elsif current_number % 3 == 0
+                puts 'Fizz'
+            elsif current_number % 5 == 0
+                puts 'Buzz' 
+            elsif current_number % 7 == 0
+                puts 'Super'
+            else
+                puts current_number
+            end
+        end
+    end
+end
